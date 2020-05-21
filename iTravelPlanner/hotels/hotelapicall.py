55b3d407c02533cebe3ffa7ee2b6842dc01a3b38
SatyanarayanaThatava <sachyat@gmail.com>
2020-05-21 22:20:45 +0530

    05/21 - iterated through hotel api response to get details for 6 hotels.

--- iTravelPlanner/hotels/hotelapicall.py
+++ iTravelPlanner/hotels/hotelapicall.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from amadeus import Client, ResponseError
@@ -9,11 +8,26 @@ amadeus = Client(
     client_id='lyGlZsuM3AYGJekRlBBjQzMxTahDnyjq',
     client_secret='3lZKe6auNLEUXrTq'
 )
-hotels = amadeus.shopping.hotel_offers.get(
+resp = amadeus.shopping.hotel_offers.get(
         cityCode='SYD')
-#print(hotels.data)
-print(hotels.data[0]['hotel']['name'])
-print(hotels.data[0]['hotel']['rating'])
+hotels = resp.data
+for i in range(6):
+    hotel_name = hotels[i]['hotel']['name']
+    hotel_addr1 = hotels[i]['hotel']['address']['lines'][0]
+    hotel_addr2 = hotels[i]['hotel']['address']['cityName']
+    hotel_addr3 = hotels[i]['hotel']['address']['stateCode']
+    hotel_addr4 = hotels[i]['hotel']['address']['countryCode']
+    hotel_addr5 = hotels[i]['hotel']['address']['postalCode']
+    hotel_curr = hotels[i]['offers'][0]['price']['currency']
+    hotel_amt  = hotels[i]['offers'][0]['price']['total']
+    print(hotel_name)
+    print(hotel_addr1)
+    print(hotel_addr2)
+    print(hotel_addr3)
+    print(hotel_addr4)
+    print(hotel_addr5)
+    print(hotel_curr)
+    print(hotel_amt)
 #hotel_offers = amadeus.shopping.hotel_offers_by_hotel.get(
 #       hotelId = 'BWSYD207', checkInDate='2020-10-10', checkOutDate='2020-10-12')
 #print(hotel_offers.data)
